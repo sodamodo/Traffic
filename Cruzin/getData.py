6b79a0db154b1e7b52f74ccbd9760ce8eeba2bac
sodamodo <lamphead@gmail.com>
2015-08-20 16:55:37 -0400

    extracted coords

--- Cruzin/getData.py
+++ Cruzin/getData.py
@@ -6,10 +6,58 @@ r = requests.get('http://207.251.86.229/nyc-links-cams/LinkSpeedQuery.txt')
 
 text = r.text
 
-format = text.split('\n')
-line = format[1].split('\t')
+lines = text.split('\n')
+linecoords = []
+jsoncoords = []
+joined = []
+row = []
+polylines = []
+for i in range(1, len(lines)-1):
+    row = lines[i].split('\t')
+    coordstring = row[6].split(' ')
+    linecoords.append(coordstring)
 
-print line
 
+for n in linecoords:
 
+    for i, z in enumerate(n):
+        print z
 
+    print n
+    polylines.append(n)
+
+for g in polylines:
+    print g
+# print polylines
+# if '"' in z:
+#             new = z.replace('"', "")
+#             n[i] = new
+#             print n[i]
+#             print new
+
+geoJson = """
+
+{ "type": "FeatureCollection",
+    "features": [
+        #features go here
+        ]
+     }
+"""
+
+linestring = """
+
+      { "type": "Feature",
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
+            ]
+          },
+        "properties": {
+          "prop0": "value0",
+          "prop1": 0.0
+         }
+        }
+      } #feature closes here
+
+          """
